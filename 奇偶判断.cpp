c92b4d448ff437b239cabe9257335820c17315ff
YYH15200 <2372698286@QQ.COM>
2026-03-23 16:19:36 +0800

    Add files via upload

---
+++ 奇偶判断.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	int x;
+	cin >> x;
+	if (x % 2 == 0) {
+		cout << "Å¼Êý" << endl;
+	}
+	else {
+		cout << "ÆæÊý" << endl;
+	}
+	return 0;
+}
